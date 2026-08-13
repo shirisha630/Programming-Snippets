b2c253cd99a0fd4af60ef969046fd51dd291a49d
shirisha <nshirisha_cse2405p8@mgit.ac.in>
2026-08-13 11:58:00 +0530

    Add Fibonacci sequence implementation in Java

---
+++ Java/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
